af39ad0e349feb4cd5c2a04f6a79564290700dd0
YazykovaDaria <yazykovadasva@gmail.com>
2025-02-05 23:02:50 +0300

    refactor: app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
+import s from './App.module.css'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Search from './components/search/Search'
+import Card from './components/card/Card'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cards, setCards] = useState([])
+  const [nextUrl, setNextUrl] = useState(null)
+
+  const setData = (cardsData) => {
+    if (cardsData) {
+      const { results } = cardsData
+      const url = cardsData.info.next
+      setCards(results)
+      setNextUrl(url)
+    } else {
+      setCards([])
+      setNextUrl(null)
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className={s.body}>
+      <Search setData={setData}></Search>
+
+      <section className={s.cardsBox}>
+        {cards.length > 0 && cards.map((card) => <Card key={card.id} {...card}></Card>)}
+      </section>
+    </main>
   )
 }
 
